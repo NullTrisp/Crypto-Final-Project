59ae62d3f53fee3905a87c9c71af15882ad614d6
Nicolas Calvache <nicolas.calvache@alumnos.uneatlantico.es>
2022-05-30 15:11:55 +0200

    added project

---
+++ Back-End/Controllers/RsaController.cs
@@ -0,0 +1,71 @@
+﻿using Back_End.Structures.Classes;
+using Back_End.Structures.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Http;
+
+namespace Back_End.Controllers
+{
+    public class RsaDecryptRequest
+    {
+        public string privateKey { get; set; }
+        public string encryptedString { get; set; }
+    }
+    public class RsaController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [Route("api/Rsa/Decrypt")]
+        public string Decrypt([FromBody] RsaDecryptRequest req)
+        {
+            try
+            {
+                return Rsa.Decrypt(req.encryptedString, req.privateKey);
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        // POST api/<controller>
+        [Route("api/Rsa/Encrypt")]
+        public string Encrypt([FromBody] RsaDecryptRequest req)
+        {
+            try
+            {
+                return Rsa.Encrypt(req.encryptedString, req.privateKey);
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Back-End/Structures/Interfaces/IPrivateKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_End.Structures.Interfaces
+{
+    public interface IPrivateKey
+    {
+        string Modulus { get; set; }
+        string Exponent { get; set; }
+        string P { get; set; }
+        string Q { get; set; }
+        string DP { get; set; }
+        string DQ { get; set; }
+        string InverseQ { get; set; }
+        string D { get; set; }
+    }
+}
