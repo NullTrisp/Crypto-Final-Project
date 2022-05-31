7e9d708ca312647fe2074a6a30df54325de007f5
Nicolás <nicolas.calvache@alumnos.uneatlantico.es>
2022-05-31 22:31:29 +0200

    final changes

--- Frontend/MainView.Designer.cs
+++ Frontend/MainView.Designer.cs
@@ -160,11 +160,11 @@ namespace Frontend
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(264, 27);
+            this.label1.Location = new System.Drawing.Point(302, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 37);
+            this.label1.Size = new System.Drawing.Size(154, 37);
             this.label1.TabIndex = 9;
-            this.label1.Text = "RSA Decryption";
+            this.label1.Text = "RSA Tool";
             // 
             // label2
             // 
